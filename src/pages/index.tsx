ca2ba598d0e8152ac50ea4e1570fd56a9b358a3d
frodehus <frode.hus@outlook.com>
2022-07-09 20:05:45 +0200

    show logged in user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
-import { UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+  useMsal,
+} from "@azure/msal-react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const { instance, accounts } = useMsal();
+  const name = accounts[0] && accounts[0].name;
 
   return (
     <div className={styles.container}>
@@ -27,21 +31,11 @@ const Home: NextPage = () => {
               Sign in
             </button>
           </UnauthenticatedTemplate>
+          <AuthenticatedTemplate>
+            <p className={styles.description}>Good to see you, {name}</p>
+          </AuthenticatedTemplate>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
